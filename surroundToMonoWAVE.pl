d2fed97c0657369b67f7f3c726659c59d968b6ea
Bangsplat <theron@dankerton.net>
2013-01-09 14:49:17 -0800

    initial commit

---
+++ surroundToMonoWAVE.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+#
+# surroundToMonoWAVE.pl
+#
+# create six mono WAVE files from a single six-channel WAVE file
+#
+# version 0
+# created 2013-01-09
+# modified 2013-01-09
+#
+
+use strict;
+use Getopt::Long;
+
+
+
+
+
+
