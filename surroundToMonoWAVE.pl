9db7db21992acaf4cfaadae77723ea126fbe1b8b
Bangsplat <theron@dankerton.net>
2013-01-10 15:21:03 -0800

    WAVE header output

--- surroundToMonoWAVE.pl
+++ surroundToMonoWAVE.pl
@@ -7,12 +7,13 @@
 #
 # version 0
 # created 2013-01-09
-# modified 2013-01-09
+# modified 2013-01-10
 #
 
 use strict;
 use Getopt::Long;
 use Time::localtime;
+use POSIX;
 
 #	constants
 
@@ -28,6 +29,7 @@ my ( $audio_format, $num_channels, $sample_rate, $byte_rate, $block_align, $bits
 my ( $sub_chunk_2_id, $sub_chunk_2_size );
 my ( @temp_file_names, @file_names, @file_handles );
 my ( $base_name, $extension );
+my ( $data_bytes_per_file, $samples_per_file );
 my @MONTHS = qw( 01 02 03 04 05 06 07 08 09 10 11 12 );
 my @DAYS = qw( 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 );
 my @HOURS = qw( 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 );
@@ -122,7 +124,6 @@ if ( $debug_param ) {
 # check this - if it does not match, warn, but continue
 if ( ( $chunk_size + 8 ) ne $file_size ) { warn "Warning: ChunkSize is not correct\n"; }
 
-
 # go find the "fmt " chunk
 
 $sub_chunk_1_id = "fmt ";
@@ -154,20 +155,29 @@ if ( $debug_param ) {
 
 # go find the "data" chunk
 $sub_chunk_2_id = "data";
-$sub_chunk_2_size = find_chunk( $sub_chunk_2_id );
+$sub_chunk_2_size = find_chunk( \*INPUT_FILE, $sub_chunk_2_id );
 if ( $sub_chunk_2_size eq 0 ) { die "Error: no data chunk\n"; }
 
-# confirm that this is a 6 channel WAVE file
+# figure out how big each file will be (the data chunk at least)
+$data_bytes_per_file = $sub_chunk_2_size / NUM_STREAMS;
+$samples_per_file = $data_bytes_per_file / ( $bits_per_sample / 8 );
 
+if ( $debug_param ) {
+	print "DEBUG: size of data chunk: $sub_chunk_2_size\n";
+	print "DEBUG: number of data bytes per output file: $data_bytes_per_file\n";
+	print "DEBUG: number of samples per output file: $samples_per_file\n";
+}
+
+# confirm that this is a 6 channel WAVE file
 if ( $num_channels ne NUM_STREAMS ) {
 	print "ERROR: wrong number of channels found\n";
 	close( INPUT_FILE );
 	exit;
 }
 
-# create temp file names
+# create file names
 $base_name = $input_param;
-$base_name =~ s/\.(.*)$//;	# lop off everything from the last period on
+$base_name =~ s/\.(.*)$//;		# lop off everything from the last period on
 $extension = $1;				# use everything after the last period as the extension
 
 if ( $debug_param ) {
@@ -175,54 +185,43 @@ if ( $debug_param ) {
 	print "DEBUG: extension: $extension\n";
 }
 
-# load @temp_file_names array with each temp file name we're going to use
+# load @file_names array with each file name we're going to use
 for ( my $i = 0; $i < NUM_STREAMS; $i++ ) {
-	@temp_file_names[$i] = "$base_name" . "_" . @channels[$i] . ".wav";
+	@file_names[$i] = "$base_name" . "_" . @channels[$i] . ".$extension";
 }
 
 if ( $debug_param ) {
 	for ( my $i = 0; $i < NUM_STREAMS; $i++ ) {
-		print "DEBUG: temp file name ($i): @temp_file_names[$i]\n";
+		print "DEBUG: file name ($i): @file_names[$i]\n";
 	}
 }
 
-# open my temp files
+# open/create the files
 for ( my $i = 0; $i < NUM_STREAMS; $i++ ) {
-	open( my $fh, '>', @temp_file_names[$i] );		# open file $i
+	open( my $fh, '>', @file_names[$i] );			# open file $i
 	binmode( $fh );									# set to binary mode
 	@file_handles[$i] = $fh;						# put handle in file_handles array
 }
 
-my $test_fh = @file_handles[0];
-
-output_header( $test_fh );
-
-print { @file_handles[1] } "test!\n";
-
-# can we pre-calculate the headers on these and write them, too?
-# that way, they don't have to be temp files
+# output a WAVE header into each file
+# each should be the same
+for ( my $i = 0; $i < NUM_STREAMS; $i++ ) {
+	output_header( @file_handles[0] );
+}
 
 # now...
-# calculate how many bytes per sample
-# 	$bits_per_sample / 8
-# calculate how many samples in the file
 # read, write, read, write, etc.
+
+# read $samples_per_file * NUM_STREAMS times
+# write $samples_per_file * NUM_STREAMS times
 
 ### Basic steps
 ###		open input file							DONE
 ###		read/parse through the header			DONE
 ###		confirm it is six channels				DONE
 ###		queue file to start of audio data		DONE
-###		create six new raw pcm (temp) files		DONE
-###		de-interleave out to temp files
-###		wrap temp files to mono WAVE files
-###		delete temp files
-###
-###	all of these steps I've done in separate scripts before
-###	but I want to combine the functionality into one here
-
-
-
+###		create six new WAVE files				DONE
+###		de-interleave out to WAVe files			
 
 
 close( INPUT_FILE );
@@ -234,54 +233,44 @@ for ( my $i = 0; $i < NUM_STREAMS; $i++ ) {
 # subroutines
 
 sub output_header {
-	print { $_[0] } "blahblahblah\n";
+	my $fh = $_[0];
 	
+	my ( $output_chunk_size, $output_sub_chunk_1_size, $output_audio_format, $output_num_channels );
+	my ( $output_sample_rate, $output_byte_rate, $output_block_align, $output_bits_per_sample );
+	
+	# chunk size next
+	#  = remainder of file from this point
+	#  = lenth of sample data + size of fmt chunk + remainder of chunk descriptor
+	#  = will generally be sample data + 36 bytes
+	$output_chunk_size = $data_bytes_per_file + 36;
+	# subChunk1Size is always 18 in Sound Forge WAV files
+	#  but as far as I can tell, it only needs to be 16
+	#  why the two pad bytes?
+	#	apparently there is an obscure Microsoft document that says
+	# 	it shall be block aligned such that it shall be 18
+	$output_sub_chunk_1_size = 16;
+	$output_audio_format = $audio_format;
+	$output_num_channels = 1;
+	$output_sample_rate = $sample_rate;
+	$output_bits_per_sample = $bits_per_sample;
+	$output_block_align = ceil( $output_num_channels * int( $output_bits_per_sample / 8 ) );
+	$output_byte_rate = $output_sample_rate * $output_block_align;
 
-# 
-# # Calculate values of WAVE header
-# 
-# my ( $chunk_size, $sub_chunk_1_size, $audio_format, $num_channels );
-# my ( $sample_rate, $byte_rate, $block_align, $bits_per_sample );
-# my ( $wav_header );
-# 
-# $wav_header = "RIFF";
-# #  = remainder of file from this point
-# #  = lenth of sample data + size of fmt chunk + remainder of chunk descriptor
-# #  = will generally be sample data + 36 bytes
-# $chunk_size = $raw_size + 36;
-# # subChunk1Size is always 18 in Forge WAV files
-# #  but as far as I can tell, it only needs to be 16
-# #  why the two pad bytes?
-# $sub_chunk_1_size = 16;
-# $audio_format = 1;
-# $num_channels = $channel_param;
-# $sample_rate = $samplerate_param;
-# $bits_per_sample = $samplesize_param;
-# $block_align = ceil( $num_channels * int( $bits_per_sample / 8 ) );
-# $byte_rate = $sample_rate * $block_align;
-# 
-# # pack()
-# # first parameter determines type of data
-# # we will be using 'S' for unsigned short and 'L' for unsigned long
-# # actually we may need to use 's' for short and 'l' for long
-# 
-# print WAV_FILE "RIFF";
-# print WAV_FILE pack( 'L', $chunk_size );
-# print WAV_FILE "WAVE";
-# 
-# print WAV_FILE "fmt ";
-# print WAV_FILE pack( 'L', $sub_chunk_1_size );
-# print WAV_FILE pack( 'S', $audio_format );
-# print WAV_FILE pack( 'S', $num_channels );
-# print WAV_FILE pack( 'L', $sample_rate );
-# print WAV_FILE pack( 'L', $byte_rate );
-# print WAV_FILE pack( 'S', $block_align );
-# print WAV_FILE pack( 'S', $bits_per_sample );
-# 
-# print WAV_FILE "data";
-# print WAV_FILE pack( 'L', $raw_size );
+	print $fh "RIFF";
+	print $fh pack( 'L', $output_chunk_size );
+	print $fh "WAVE";
 
+	print $fh "fmt ";
+	print $fh pack( 'L', $output_sub_chunk_1_size );
+	print $fh pack( 'S', $output_audio_format );
+	print $fh pack( 'S', $output_num_channels );
+	print $fh pack( 'L', $output_sample_rate );
+	print $fh pack( 'L', $output_byte_rate );
+	print $fh pack( 'S', $output_block_align );
+	print $fh pack( 'S', $output_bits_per_sample );
 
+	print $fh "data";
+	print $fh pack( 'L', $data_bytes_per_file );
 }
 
 sub find_chunk {
@@ -289,10 +278,15 @@ sub find_chunk {
 	my $find_chunk_id = $_[1];
 	my $done = 0;
 	my ( $result, $buffer, $result, $read_chunk_id, $read_chunk_size );
-	 
+	
+	if ( $debug_param ) {
+		print "DEBUG (find_chunk): looking in file $file_handle\n";
+		print "DEBUG (find_chunk): looking for $find_chunk_id chunk\n";
+	}
+	
 	# assume that $file_handle is an open file
 	
-	seek( $file_handle, 12, 0 );		# skip past the end of the header
+	seek( $file_handle, 12, 0 );		# start just past the end of the header
 	
 	while ( !$done ) {
 		$result = read( $file_handle, $buffer, 8 );		# read the header of the next chunk
